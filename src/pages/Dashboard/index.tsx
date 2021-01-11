d0e8d349c15995520e6cf0123e4058e22f6e868e
vitorcrl <vitor.carlos_souza@hotmail.com>
2021-01-11 02:36:26 -0300

    Create interface from explorer page 1

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,41 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o Nome do Repositorio." />
         <button type="submit">Pesquisar</button>
       </Form>
+      <Repositories>
+        <a href="https://www.twitch.tv/colonogamer">
+          <img
+            src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b1/b1b8036021c6a3bdc8951ca95b5112f94f133957_full.jpg"
+            alt="Vitor Carlos"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy Peasy Lemon Squeazy</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="https://www.twitch.tv/colonogamer">
+          <img
+            src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b1/b1b8036021c6a3bdc8951ca95b5112f94f133957_full.jpg"
+            alt="Vitor Carlos"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy Peasy Lemon Squeazy</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="https://www.twitch.tv/colonogamer">
+          <img
+            src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b1/b1b8036021c6a3bdc8951ca95b5112f94f133957_full.jpg"
+            alt="Vitor Carlos"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy Peasy Lemon Squeazy</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };

9c0ae0d7ca934ca40a3b0faa3f7b14f7c98b6d7a
vitorcrl <vitor.carlos_souza@hotmail.com>
2021-01-12 03:06:08 -0300

    create navigation from routes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface Repositoryparams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<Repositoryparams>();
+  return <h1>Repository:{params.repository}</h1>;
 };
 
 export default Repository;
